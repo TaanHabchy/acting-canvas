756d0ad5b11a43a22fb254ec953d1fb357d6ab96
Sharbel Habchy <sharbelhabchy@gmail.com>
2025-11-03 14:56:52 -0600

    basic basename

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/acting-canvas">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/resume" element={<Resume />} />
